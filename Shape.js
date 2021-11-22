0e11e4654e653cda9f6ea9f5ead33ffcc62ecefc
KV-Hemanth <93839727+KV-Hemanth@users.noreply.github.com>
2021-11-22 21:34:59 +0530

    Add files via upload

---
+++ Shape.js
@@ -0,0 +1,18 @@
+// declare class
+class Shape{
+  constructor(color){
+    this.color= 'color';
+  }
+
+     drawShape()
+     {
+    console.log('Shape');
+     }
+  calculateArea(){
+    console.log('Area');
+     }
+}
+    module.exports = Shape;
+// export class using module.exports
+  
+//
